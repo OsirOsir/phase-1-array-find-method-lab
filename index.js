57f4140e1c738cd02adda7d81dacc35be4f45419
Philip osir Otieno <philiposir2@gmail.com>
2024-07-01 19:24:56 +0300

    added the .find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(collection) {
+ const yearW = collection.find(years => years.result === "W")
+
+ if( yearW) {
+  return yearW.year
+ } else {
+  return undefined
+ }
+}
+// console.log(superbowlWin(record));
